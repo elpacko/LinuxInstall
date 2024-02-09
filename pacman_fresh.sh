71bb85007c6bdbb4cdd9f3bbd36ec437fcd71685
Francisco Javier Rivera Lujan <elpacko@gmail.com>
2024-02-08 18:22:29 -0700

    Create pacman_fresh.sh
    init on pacman fresh

---
+++ pacman_fresh.sh
@@ -0,0 +1,10 @@
+# if pacman
+sudo pacman -Syu
+sudo pacman -S neovim neofetch btop htop pipx yay fish base-devel jq aws-cli fzf wget git
+chsh -s /usr/bin/fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install lambda
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install PatrickF1/fzf.fish
+pipx install virtualfish
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
