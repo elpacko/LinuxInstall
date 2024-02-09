1e159c1323cea9a050722f67c8b02b002cc05ba8
Francisco Javier Rivera Lujan <elpacko@gmail.com>
2024-02-08 19:48:54 -0700

    Update pacman_fresh.sh
    updated pacman packages

--- pacman_fresh.sh
+++ pacman_fresh.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # if pacman
 sudo pacman -Syu
-sudo pacman -S neovim neofetch btop htop fish base-devel jq aws-cli fzf wget git
+sudo pacman -S neovim neofetch btop htop fish base-devel jq aws-cli fzf wget git openssh python-pipx
 chsh -s /usr/bin/fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install lambda
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
-pipx install virtualfish
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+pipx install virtualfish
