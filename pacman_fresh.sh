e9ff2f6fc1fec1f7374c9f3a991f11374028cc9e
Francisco Javier Rivera Lujan <elpacko@gmail.com>
2024-04-30 16:37:25 -0600

    Added colored cat and nvchad url fix

--- pacman_fresh.sh
+++ pacman_fresh.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # if pacman
 sudo pacman -Syu
-sudo pacman -S neovim neofetch btop htop fish base-devel jq aws-cli fzf wget git openssh python-pipx
+sudo pacman -S neovim neofetch btop htop fish base-devel jq aws-cli fzf wget git openssh python-pipx less pygmentize
 chsh -s /usr/bin/fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install lambda
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+pipx ensurepath
+exec fish
 pipx install virtualfish
+pipx install poetry
+alias ccat='pygmentize -g'
